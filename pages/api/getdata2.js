ab7fab57633ec3c1add7818b9377f099ca6b3da0
zhangwer <francezw@hotmail.com>
2021-08-30 22:41:19 +0800

    1.0.0

--- pages/api/getdata2.js
+++ pages/api/getdata2.js
@@ -1,13 +1,10 @@
-import { connectToDatabase } from './mongodb';
-export default async function getdata (req, res) {
-  let query = {}
-  let options = {}
-  let { db } = await connectToDatabase()
-  // let { body } = req
-  // const bdcollection = body.collection
-  const collection = await db.collection('index')
-  const result = await collection.find(query,options).toArray()
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  res.status(200).json(result);
-  return
-}
+import  clientPromise  from "./lib/mongodb";
+export default async (req, res) => {
+  const client = await clientPromise;
+  const movies = await client.db('unio')
+    .collection("index")
+    .find({})
+    .limit(20)
+    .toArray();
+  res.json(movies);
+};
